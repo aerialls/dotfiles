7da2ef097cf88add5c3f0979f3180385324acc3d
Julien Brochet <julien@aerialls.io>
2018-01-22 22:21:06 +0100

    Add custom alias 'git-sync-upstream'

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,5 @@
 if is_macos; then
     alias afk="pmset displaysleepnow"
 fi
+
+alias git-sync-upstream="git checkout master && git fetch upstream && git merge upstream/master && git push origin master"
