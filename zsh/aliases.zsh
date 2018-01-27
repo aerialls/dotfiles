81b482438e3a5bec3be8f84a9d29556c9b9987d0
Julien Brochet <julien@aerialls.io>
2018-01-27 11:27:24 +0100

    Remove unneeded git alias anymore

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,3 @@
 if is_macos; then
     alias afk="pmset displaysleepnow"
 fi
-
-alias git-sync-upstream="git checkout master && git fetch upstream && git merge upstream/master && git push origin master"
