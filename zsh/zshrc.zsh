6c42e289816da19b61db966dfde7905224fa9bf9
Julien Brochet <julien@aerialls.io>
2018-09-30 21:02:40 +0200

    Update everything

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,42 +1,39 @@
+export LANG=en_US.UTF-8
+
 source ~/dotfiles/zsh/functions.zsh
 source ~/dotfiles/zsh/aliases.zsh
 
-source ~/dotfiles/zgen/zgen.zsh
-
-if ! zgen saved; then
-    zgen oh-my-zsh
-
-    zgen oh-my-zsh plugins/brew
-    zgen oh-my-zsh plugins/git
-    zgen oh-my-zsh plugins/git-extras
-    zgen oh-my-zsh plugins/docker
-
-    zgen load unixorn/git-extra-commands
-
-    if is_macos; then
-        zgen load chriskempson/base16-iterm2
-    fi
-
-    zgen oh-my-zsh themes/agnoster
-
-    zgen save
-fi
+export ZPLUG_HOME=/usr/local/opt/zplug
+source $ZPLUG_HOME/init.zsh
 
 # Custom settings pear workstation
 HOSTNAME="$(hostname -s)"
+
+zplug "dracula/zsh", as:theme
+
+zplug "plugins/common-aliases", from:oh-my-zsh
+zplug "plugins/brew", from:oh-my-zsh
+zplug "plugins/git", from:oh-my-zsh
+zplug "plugins/docker", from:oh-my-zsh
 
 if [ -f ~/dotfiles/zsh/local/$HOSTNAME.zsh ]; then
     source ~/dotfiles/zsh/local/$HOSTNAME.zsh
 fi
 
+# Install missing plugins
+if ! zplug check --verbose; then
+  printf "Install? [y/N]: "
+  if read -q; then
+    echo; zplug install
+  fi
+fi
+
+zplug load
+
 if is_macos; then
     export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
-    export PATH="$PATH:/usr/local/opt/python@2/bin"
 fi
 
 # Go specific stuff
 export GOPATH="$HOME/Go"
 export PATH="$PATH:$HOME/Go/bin"
-
-# Remove prompt
-prompt_context() {}
