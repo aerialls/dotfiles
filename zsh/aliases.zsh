eb7a6a8e225fbb0ff33873c548e4ba7b30bea803
Julien Brochet <julien@aerialls.io>
2019-08-07 21:53:09 +0200

    feat(k8s): add some aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,13 @@
-alias afk="pmset displaysleepnow"
-alias git=hub
-alias k=kubectl
+alias git='hub'
+alias k='kubectl'
+
+alias kgp='kubectl get pods'
+alias kgs='kubectl get services'
+alias kgs='kubectl get services'
+alias kgd='kubectl get deployments'
+alias kgc='kubectl get componentstatuses'
+
+alias kcuc='kubectl config use-context'
+alias kcsc='kubectl config set-context'
+alias kcdc='kubectl config delete-context'
+alias kccc='kubectl config current-context'
