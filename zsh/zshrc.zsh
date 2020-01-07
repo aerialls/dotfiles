1f66bdc34971521e73021bf2882576bce71ba580
Julien Brochet <julien@aerialls.io>
2020-01-07 10:31:25 +0100

    feat(brew): add Python 3.8 support

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -4,11 +4,14 @@ export HOSTNAME="$(hostname -s)"
 source ~/dotfiles/prezto/init.zsh
 source ~/dotfiles/zsh/aliases.zsh
 
+# Go
 export GOPATH="$HOME/Go"
-
 export PATH="$HOME/Go/bin:$PATH"
+
+# Others
 export PATH="/usr/local/opt/mysql-client/bin:$PATH"
 export PATH="/usr/local/opt/ruby/bin:$PATH"
+export PATH="/usr/local/opt/python@3.8/bin:$PATH"
 
 if [[ -n $(find ~/dotfiles/zsh/local/ -name "$HOSTNAME*.zsh") ]]; then
     source ~/dotfiles/zsh/local/$HOSTNAME*.zsh
